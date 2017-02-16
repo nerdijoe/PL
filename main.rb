afaf473d1ab38e3025e62d39bc4ff946b519ac74
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-02-17 00:16:02 +0800

    create class Car and ParkingLot

--- main.rb
+++ main.rb
@@ -1,1 +1,20 @@
 # main
+
+class Car
+  def initialize(**input)
+    @reg_num = input.fetch(:reg_num)
+    @colour = input.fetch(:colour)
+  end
+
+
+end
+
+
+class ParkingLot
+  def initialize(**input)
+    @num_slots = input.fetch(:num_slots)
+    @slots = nil
+    @file = input.fetch(:file)
+  end
+
+end
